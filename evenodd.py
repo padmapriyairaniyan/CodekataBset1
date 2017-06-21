97ffac96e2d937399b83c6c59c6674c0f4068e5c
padmapriyairaniyan <padmapriya7897@gmail.com>
2017-06-21 10:22:29 +0530

    Create evenodd.py

---
+++ evenodd.py
@@ -0,0 +1,11 @@
+def main():
+    n=int(input("enter a number"))
+    if(n%2==0):
+        print ("even")
+    elif(n<=0):
+        print ("invalid number")
+    else:
+        print ("odd")
+
+if __name__ == '__main__':
+    main()
