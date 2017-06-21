b0b85c2927c56f5aaebae76c3d1bb4b74962b79b
padmapriyairaniyan <padmapriya7897@gmail.com>
2017-06-21 12:10:44 +0530

    Create amstrong1.py

---
+++ amstrong1.py
@@ -0,0 +1,13 @@
+n=int(input("enter a number"))
+n1=n
+n1=str(n1)
+l=len(n1)
+result=0
+while n!=0:
+    r=n%10
+    result+=(r**l)
+    n=n//10
+if(int(n1)==result):
+    print("Amstrong number")
+else:
+    print("Not an Amstrong Number")
